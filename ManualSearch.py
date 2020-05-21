fcadd66807b12696ff8b03ab5482b53b4912faa1
MylesACD <49326725+MylesACD@users.noreply.github.com>
2020-05-21 16:10:00 -0700

    first commit

--- ManualSearch.py
+++ ManualSearch.py
@@ -4,24 +4,27 @@ import io
 import pydotplus
 from IPython.display import display, Image
 from rdflib.tools.rdf2dot import rdf2dot
-
+import os
+os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
 
 g = Graph()
 j = Graph()
-
+n=0
 g.parse("SugarScape.ttl", format="turtle")
     # loop through each triple in the graph (subj, pred, obj)
-n= 0
-for subj, pred, obj in g:
-    if subj == 'file:///C:/Users/Myles/PycharmProjects/untitled/vDist_read#id=baed4b73-b7b6-4f01-a7be-60d567c76641':
-        j.add([subj,pred,obj])
 
 
-    if n==100:
-        j.add([subj, pred, obj])
-        n=0
-    else:
-        n+=1
+    #"vDist_read#id=baed4b73-b7b6-4f01-a7be-60d567c76641")
+
+    # if subj == 'file:///C:/Users/Myles/PycharmProjects/untitled/vDist_read#id=baed4b73-b7b6-4f01-a7be-60d567c76641':
+    #     j.add([subj,pred,obj])
+
+for triple in g:
+     if n==15000:
+         j.add(triple)
+         n=0
+     else:
+         n+=1
 
 print("graph has {} statements.".format(len(j)))
 
@@ -29,6 +32,7 @@ print("graph has {} statements.".format(len(j)))
 
 
 def visualize(g):
+
     stream = io.StringIO()
     rdf2dot(g, stream, opts = {display})
     dg = pydotplus.graph_from_dot_data(stream.getvalue())
