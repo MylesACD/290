5034caa53960efc4141f76b9ee1269e6f597e928
MylesACD <49326725+MylesACD@users.noreply.github.com>
2020-05-21 22:28:56 -0700

    ready for demo

--- ManualSearch.py
+++ ManualSearch.py
@@ -1,41 +1,70 @@
-import rdflib
+import tkinter
+from tkinter import filedialog
 from rdflib import Graph
+from tkinter import *
+from tkinter import simpledialog
 import io
 import pydotplus
-from IPython.display import display, Image
+from IPython.display import display
 from rdflib.tools.rdf2dot import rdf2dot
 import os
+
 os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
+master = Tk()
+master.geometry("300x300")
 
 g = Graph()
 j = Graph()
-n=0
-g.parse("SugarScape.ttl", format="turtle")
-    # loop through each triple in the graph (subj, pred, obj)
 
 
-    #"vDist_read#id=baed4b73-b7b6-4f01-a7be-60d567c76641")
-
-    # if subj == 'file:///C:/Users/Myles/PycharmProjects/untitled/vDist_read#id=baed4b73-b7b6-4f01-a7be-60d567c76641':
-    #     j.add([subj,pred,obj])
-
-for triple in g:
-     if n==15000:
-         j.add(triple)
-         n=0
-     else:
-         n+=1
 
 print("graph has {} statements.".format(len(j)))
 
 
+def selectFile():
+    master.fileName = filedialog.askopenfilename(filetypes=(("turtle files", ".ttl"), ("All files", "*")))
+    g.parse(master.fileName, format="turtle")
+    print("done parsing")
 
 
-def visualize(g):
+def closeWindows():
+    exit()
 
+
+def visualize():
+    n = 0
+    for triple in g:
+        if n < c:
+            j.add(triple)
+            n += 1
+        else:
+            break
     stream = io.StringIO()
-    rdf2dot(g, stream, opts = {display})
+    rdf2dot(j, stream, opts={display})
     dg = pydotplus.graph_from_dot_data(stream.getvalue())
-    dg.write_svg("test.svg")
+    dg.write_svg("test" + str(c) + ".svg")
 
-visualize(j)
+
+def tripleCount():
+    global c
+    c = tkinter.simpledialog.askinteger("Data","How many triples do you want to read?")
+
+
+output = Label(master, text="Simulation Visualizer")
+output.pack()
+
+button3 = Button(master, text="Select File", command=selectFile)
+
+button3.pack()
+
+button = Button(master, text="Graph Data", command=visualize)
+
+button.pack()
+
+button2 = Button(master, text="Quit", command=closeWindows)
+
+button4 = Button(master, text="Triple Count", command=tripleCount)
+button4.pack()
+button2.pack()
+
+mainloop()
